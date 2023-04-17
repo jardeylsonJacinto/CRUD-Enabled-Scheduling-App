32aa31679d69de8b2ab2ce18e9ec62c92574d4b9
Jardeylson jacinto <jardeylsong.m@gmail.com>
2023-04-17 14:32:33 -0300

    fix: a nug fix in the server

--- backend/src/routes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const routes = Router();
-
-routes.get('', (req, res) => {
-  res.json({ message: "Welcome to Scheduler back-end service."});
-})
-
-module.exports = { routes };
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,28 +1,27 @@
+let corsOptions = { origin: "http://localhost:3333" };
 const bodyParser = require("body-parser");
-const { routes } = require("./routes");
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
-let corsOptions = { origin: "http://localhost:3333"};
-const PORT = process.env.PORT || 3333;
-
-const db = require("../models");
-db.sequelize.sync({ force: false }).then(() => {
-  console.log("Drop and re-sync db.");
-});
-
-require("./routes/scheduler.routes")(app);
 // set port, listen for requests
+const PORT = process.env.PORT || 3334;
+
+app.use(cors(corsOptions));
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Scheduler backend service." });
+});
+require("./routes/scheduler.routes")(app);
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
-app.use(cors(corsOptions));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/', routes);
-
-app.listen(PORT, () =>{
-  console.log(`Server is runnig on port ${PORT}`);
-})
+const db = require("./models");
+db.sequelize.sync({ force: false }).then(() => {
+  console.log("Drop and re-sync db.");
+});
