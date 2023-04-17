74714b4ea6d082a3dd09c76ea27597cdd11c1921
Jardeylson jacinto <jardeylsong.m@gmail.com>
2023-04-17 11:41:47 -0300

    feat: Initializing Sequelize in the model

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,16 @@
-const { app } = require('./index');
+const bodyParser = require("body-parser");
+const { routes } = require("./routes");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+let corsOptions = { origin: "http://localhost:3333"};
 const PORT = process.env.PORT || 3333;
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', routes);
 
 app.listen(PORT, () =>{
   console.log(`Server is runnig on port ${PORT}`);
